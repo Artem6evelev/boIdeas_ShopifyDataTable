57b5c01e5e6f86abecb6a30b2560e8fd6b58c429
Артем Шевелев <artemsevelev@Artems-MacBook-Pro.local>
2021-08-01 10:26:44 +0300

    create client and table template

---
+++ src/components/columns.js
@@ -0,0 +1,30 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        accessor: 'id'
+    },
+    {
+        Header: 'Shopify Order Number',
+        accessor: 'shopify_order_number'
+    },
+    {
+        Header: 'Customer Full Name',
+        accessor: 'customer_full_name'
+    },
+    {
+        Header: 'Order date',
+        accessor: 'order_date'
+    },
+    {
+        Header: 'Order summary amount',
+        accessor: 'order_summary_amount'
+    },
+    {
+        Header: 'Line Item Title',
+        accessor: 'line_item_title'
+    },
+    {
+        Header: 'price',
+        accessor: 'price'
+    }
+]
